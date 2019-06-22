aae076803266de497796f274180ea70984cda168
jpc0016 <jpc0016@uah.edu>
2019-06-22 17:27:22 -0500

    nc progress

--- nc-client.py
+++ nc-client.py
@@ -17,4 +17,21 @@ port = 0
 
 # Show usage of the function if arguments are not provided
 def usage():
-    print("BHP Net Tool")
+  print("BHP Net Tool")
+  print("")
+  print("Usage: nc-client.py -t target_host -p port")
+  print("-l --listen    - listen on [host]:[port] for incoming connection")
+  print("-e --execute=file-to-run   - execute the given file upon receiving a connection")
+
+  sys.exit(0)
+
+def main():
+  global listen
+  global port
+  global execute
+  global command
+  global upload_destination
+  global target
+
+  if not len(sys.argv[1:]):
+    usage()
