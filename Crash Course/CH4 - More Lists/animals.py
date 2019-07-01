25174e2a8388e7cf946b63b99a5630f480af869d
jpc0016 <jpc0016@uah.edu>
2019-06-30 22:29:18 -0500

    more ch4

---
+++ Crash Course/CH4 - More Lists/animals.py
@@ -0,0 +1,14 @@
+# CH4 Exercise 4-2
+#
+# animals.py
+#
+# Think of 3 different animals with a common characteristic.  Store
+# these names in a list
+animals = ['shark', 'alligator', 'bear']
+
+# Use a for loop to print each name
+for animal in animals:
+    print("A " + animal + " is a scary animal!")
+
+# Add a line stating what these animals have in common.
+print("\nThese are all terrifying pets!")
---
+++ Crash Course/CH4 - More Lists/pizza.py
@@ -0,0 +1,14 @@
+# CH4 Exercise 4-1
+#
+# pizzas.py
+# Three favorite pizzas in a list.
+pizzas = ['cheese', 'pepperoni', 'supreme']
+
+# Modify your for loop to print a sentence using the name of the pizza
+# instead of printing just the name of the pizza.
+for pizza in pizzas:
+    print("I love " + pizza + " pizza!")
+
+# Add a line at the end of your program, outside the for loop, that states
+# how much you like pizza.
+print("\nYou can tell I really enjoy pizza!")
