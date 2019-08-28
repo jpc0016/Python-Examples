2141c1be78ae5ed091c9863e313096a28de7bfe9
John Cox <jcox@battlestation.com>
2019-08-28 16:08:27 -0500

    added exception techniques

--- Crash Course/Project2-Data-Visualization/ch16/high_lows.py
+++ Crash Course/Project2-Data-Visualization/ch16/high_lows.py
@@ -7,11 +7,13 @@
 import csv
 from datetime import datetime
 
-# Run in Python2.7 until matplotlib is fixed for Python3.7
+# Can pass multiple errors in a tuple as 'error' or 'e'
+# use quit(0) to cut out traceback messages after user created message
 try:
     import matplotlib.pyplot as plt
-except:
+except (NameError, ImportError, ModuleNotFoundError) as e:
     print("Cannot import matplotlib for some reason...")
+    quit(0)
 
 # Use full dataset once testing complete on sample set
 filename = 'data/sitka_weather_2018_full.csv'
@@ -25,24 +27,25 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
 
     for row in reader:
-        # convert each element into an integer before storing into the list. This is
-        # to prepare data for matplotlib
+        # convert each element into an integer before storing into the list.
+        # This is to prepare data for matplotlib
         try:
+            date = datetime.strptime(row[2], '%Y-%m-%d')
             high = int(row[8])
             low = int(row[9])
-        except:
-            high = 0
-            low = 0
-        highs.append(high)
-        lows.append(low)
+        except ValueError:
+            print(date, 'missing data')
+        else:
+            dates.append(date)
+            highs.append(high)
+            lows.append(low)
 
-        date = datetime.strptime(row[2], '%Y-%m-%d')
-        dates.append(date)
 
     #print(highs)
 
     # Plot data points in a temperature chart (normal line chart)
-    # plt.figure() customizes the plot figure instead of using default plt.plot values
+    # plt.figure() customizes the plot figure instead of using default
+    # plt.plot values
     fig = plt.figure(dpi=128, figsize=(10, 6))
     plt.plot(dates, highs, c='red', alpha=0.5)
     plt.plot(dates, lows, c='blue', alpha=0.5)
@@ -50,7 +53,7 @@ with open(filename) as f:
     plt.fill_between(dates, highs, lows, facecolor='red', alpha=0.1)
 
     # Format plot
-    plt.title("Daily High and Low Temperatures - 2018", fontsize=24)
+    plt.title("Daily High and Low Temperatures - 2018\nSitka, Alaska", fontsize=20)
     plt.xlabel('', fontsize=16)
     # Auto-format dates
     fig.autofmt_xdate()
@@ -59,7 +62,7 @@ with open(filename) as f:
 
     plt.show()
 
-    # enumerate() returns an index of each item in header_row.  Very handy in
-    # creating numbered lists.
+    # enumerate() returns an index of each item in header_row.  Very handy
+    # in creating numbered lists.
     # for index, column_header in enumerate(header_row):
     #     print(index, column_header)
