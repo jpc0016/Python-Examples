e2e50b1286cc0da3893a314891a6036af6b275b8
jpc0016 <jpc0016@uah.edu>
2019-08-30 20:49:57 -0500

    Project 1 CS585 works!

--- Code-Cracking/ch18-Vigenere/vigenere-working.py
+++ Code-Cracking/ch18-Vigenere/vigenere-working.py
@@ -1,6 +1,12 @@
 # CH18 Example
 #
 # vigenere-working.py
+#
+# v1.0
+#
+# Both encrypt() and decrypt() functions work on strings of same size.
+# Current file aiming on variable length keys
+#
 # Demonstrate the Vigenere cipher on an input string and display its output.
 # Program must encrypt and decrypt strings of variable length
 #import pyperclip
@@ -39,9 +45,10 @@ character_key = {
 plaintext = input("Enter the plaintext string to encrypt: ")
 # 'hello'
 vigenere = input("Enter the vigenere key: ")
-# 'pizza'
+# 'dog'
 
 ##### CURRENTLY ONLY WORKS FOR STRINGS OF SAME SIZE #####
+#### WORKING ON ENCRYPTING OVER VARIABLE KEY LENGTHS ####
 def encrypt(plaintext, vigenere):
     """
     Encrypts plaintext using a vigenere key. Each character of the alphabet
@@ -55,7 +62,8 @@ def encrypt(plaintext, vigenere):
         plain_key = character_key[char.upper()]
         plain_keys.append(plain_key)
 
-    # Enumerate characters of the key and store in vigenere_keys
+    # Enumerate characters of the key and store in vigenere_keys.  Enumerated
+    # keys length should match plain_keys length
     vigenere_keys = []
     for item in vigenere:
         vigenere_key = character_key[item.upper()]
@@ -68,10 +76,40 @@ def encrypt(plaintext, vigenere):
     # [7, 4, 11, 11, 14]
     print("\nEnumerated Key")
     print(vigenere_keys)
-    # [15, 8, 25, 25, 0]
+    # [3, 14, 6]
 
-    # Add the elements of each array together and mod 26
+    len_plain = len(plain_keys)
+    # 5
+    len_vig = len(vigenere_keys)
+    # 3
+    mod = len_plain % len_vig
+    # 2
+
+    # (len_plain - mod) / len_vig = number of iterations to fill vigenere_keys
+
+    # Extend vigenere_keys to match plain_keys in length.  Check if
+    # vigenere_keys is shorter than plain_keys
+    if len_vig < len_plain:
+        # (5 - 2) / 3 = 3/3 = 1
+        iterations =  (len_plain - mod) / len_vig
+        for iter in range(0, int(iterations)):
+            for j in range(0, len_vig):
+                if len(vigenere_keys) == len(plain_keys):
+                    break
+                else:
+                    vigenere_keys.append(vigenere_keys[j])
+
+    # If plain_keys < vigenere_keys, stop encryption at end of plain_keys
+    # Entire program works if above condition is met
+    print("\nPadded Vigenere Key: ")
+    print(vigenere_keys)
+
+
+    # Add the elements of each array together and mod 26.  If len(vigenere) is
+    # less than len(plaintext), restart at begining of key.  If len(vigenere)
+    # is greater than len(plaintext), stop at end of plaintext
     # Result is encrypted_numbers list
+
     encrypted_numbers = []
     for i in range(0, len(plain_keys)):
         new = (plain_keys[i] + vigenere_keys[i]) % 26
@@ -109,17 +147,86 @@ def decrypt(ciphertext, vigenere):
         cipher_key = character_key[char.upper()]
         cipher_keys.append(cipher_key)
 
-    # Troubleshooting:
-    # Show cipher keys [GOOD]
-    print("Cipher Keys:")
-    print(cipher_keys)
+
+    # Enumerate characters of the vigenere key
+    vigenere_keys = []
+    for item in vigenere:
+        vigenere_key = character_key[item.upper()]
+        vigenere_keys.append(vigenere_key)
+
+    len_cipher = len(cipher_keys)
+    # 5 (same as len(plain_keys))
+    len_vig = len(vigenere_keys)
+    # 3
+    mod = len_cipher % len_vig
+    # 2
+
+    # (len_cipher - mod) / len_vig = number of iterations to fill vigenere_keys
+
+    # Extend vigenere_keys to match cipher_keys in length.  Check if
+    # vigenere_keys is shorter than cipher_keys
+    if len_vig < len_cipher:
+        # (5 - 2) / 3 = 3/3 = 1
+        iterations =  (len_cipher - mod) / len_vig
+        for iter in range(0, int(iterations)):
+            for j in range(0, len_vig):
+                if len(vigenere_keys) == len(cipher_keys):
+                    break
+                else:
+                    vigenere_keys.append(vigenere_keys[j])
+
+    # If cipher_keys < vigenere_keys, stop encryption at end of plain_keys
+    # Entire program works if above condition is met
+    print("\nPadded Vigenere Key: ")
+    print(vigenere_keys)
+
+    # # Troubleshooting:
+    # # Show cipher keys [GOOD]
+    # print("\nCipher Keys:")
+    # print(cipher_keys)
+    #
+    # # Show vigenere keys [GOOD]
+    # print("\nVigenere Keys:")
+    # print(vigenere_keys)
+
+    # Subtract the elements of each array and mod 26
+    # Result is decrypted_numbers list
+    decrypted_numbers = []
+    for i in range(0, len(cipher_keys)):
+        # Need to handle instance where cipher - key < 0
+        # Example: B:1 - F:5 = -4
+        if (cipher_keys[i] - vigenere_keys[i] < 0):
+            new = 26 - (vigenere_keys[i] - cipher_keys[i])
+        else:
+            # W:22 - P:15 = H:7
+            new = (cipher_keys[i] - vigenere_keys[i]) % 26
+        decrypted_numbers.append(new)
+
+    # Convert enumerated ciphertext - key into plaintext string. Initialize
+    # empty string
+    decrypted = ""
+    for number in decrypted_numbers:
+        # for 22
+        for key, value in character_key.items():
+            # for 'A':0 , 'B':1, ...
+            # print(key) # A
+            # print(value) # 0
+            if value == number:
+                # if 0 = 22
+                decrypted += key
+    return decrypted
 
 
 
 
 # Test encrypt() function
 crypto = encrypt(plaintext, vigenere)
-print("Encrypted text: " + crypto)
+print("\n####################################")
+print("\nEncrypted text: " + crypto)
+print("\n####################################")
 
 # Test decrypt() function
-decrypt(crypto, vigenere)
+plain = decrypt(crypto, vigenere)
+print("\n####################################")
+print("\nRecovered plaintext: " + plain)
+print("\n####################################")
